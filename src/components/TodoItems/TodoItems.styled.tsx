94cedd84d8e43ac668afa981401acda5bc57c8be
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2023-08-05 23:20:24 +0300

    style modal

--- src/components/TodoItems/TodoItems.styled.tsx
+++ src/components/TodoItems/TodoItems.styled.tsx
@@ -1,6 +1,36 @@
 import styled from '@emotion/styled';
+import { RxCross1 } from 'react-icons/rx';
 
 export const Items = styled.li`
   display: flex;
   list-style: none;
+`;
+
+export const ModalWraper = styled.div`
+  position: relative;
+  width: 500px;
+  height: 500px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset,
+    rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+`;
+
+export const Title = styled.h1`
+  margin-bottom: 20px;
+  text-align: center;
+`;
+
+export const Icon = styled(RxCross1)`
+  position: absolute;
+  right: 12px;
+  top: 12px;
+  cursor: pointer;
+  transition: scale 250ms cubic-bezier(0.075, 0.82, 0.165, 1);
+
+  :hover {
+    color: #1976d2;
+    scale: 1.2;
+  }
 `;
--- src/components/TodoItems/TodoItems.tsx
+++ src/components/TodoItems/TodoItems.tsx
@@ -2,8 +2,9 @@ import { useAppDispatch } from '../../hooks/redux.hooks';
 import { deleteTodo, getOneTodo, clearOneTodo } from '../../redux/todo/todoOperations';
 import Modal from '../Modal/Modal';
 import { ITodoProps } from '../../interface/todo.interface';
-import { Items } from './TodoItems.styled';
 import { useCustomSelector } from '../../redux/selectors';
+
+import { Items, ModalWraper, Title, Icon } from './TodoItems.styled';
 
 const TodoItem = ({ items }: ITodoProps) => {
   const dispatch = useAppDispatch();
@@ -11,6 +12,7 @@ const TodoItem = ({ items }: ITodoProps) => {
 
   const handleDelete = (id: string) => {
     void dispatch(deleteTodo(id));
+    dispatch(clearOneTodo());
   };
 
   const handleView = (id: string) => {
@@ -31,7 +33,19 @@ const TodoItem = ({ items }: ITodoProps) => {
           </button>
           {Object.keys(todo).length > 0 && (
             <Modal onClick={() => dispatch(clearOneTodo())}>
-              <p>Modal</p>
+              <ModalWraper>
+                <Icon onClick={() => dispatch(clearOneTodo())} />
+                <Title>Todo modal</Title>
+                <p>
+                  <b>Title:</b> {title}
+                </p>
+                <p>
+                  <b>Description:</b> {description}
+                </p>
+                <button type="button" onClick={() => handleDelete(_id)}>
+                  Delete
+                </button>
+              </ModalWraper>
             </Modal>
           )}
         </Items>
